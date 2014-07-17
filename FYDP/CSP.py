c5192ae6613f03e51f3a11c3b2ef8b72dceabdc3
Justin Williams <justin.graham49@gmail.com>
2014-07-17 15:35:53 -0400

    Fixed function docstrings

--- FYDP/CSP.py
+++ FYDP/CSP.py
@@ -15,12 +15,14 @@ def create_e_matrix(channelMap):
 
 
 def generate_sigma_matrix(channelMaps) :
-	''' channelMaps should contain all the training sets for a specific cognitive function
-	return : sigma matrix for the specific cognitive function'''
+	""" channelMaps should contain all the training sets for a specific cognitive function
+	return : sigma matrix for the specific cognitive function
+	"""
 	pass
 
 def find_omega_matrix(sigmaMatrices) :
-	''' sigmaMatrices should contain all the sigma matrices for the specific cognitive functions to be compared
-	return : omega matrix'''
+	""" sigmaMatrices should contain all the sigma matrices for the specific cognitive functions to be compared
+	return : omega matrix
+	"""
 	pass
 
