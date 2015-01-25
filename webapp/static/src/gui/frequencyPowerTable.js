e6fe5906a73e55eeb1f5bdbda83efcf77dbd2ddf
Justin Williams <justin.graham49@gmail.com>
2015-01-24 20:13:58 -0500

    Implemented method to update the chart for the corresponding signal.

--- webapp/static/src/gui/frequencyPowerTable.js
+++ webapp/static/src/gui/frequencyPowerTable.js
@@ -6,15 +6,15 @@ define('FrequencyPowerTable', ['jquery', 'SignalNameEnum', 'Chart', 'lodash'], f
         }
         this.signalName = signalName;
         this.deltaPower = null;
-        this.deltaSignal = null;
+        this.deltaChart = null;
         this.thetaPower = null;
-        this.thetaSignal = null;
+        this.thetaChart = null;
         this.alphaPower = null;
-        this.alphaSignal = null;
+        this.alphaChart = null;
         this.betaPower = null;
-        this.betaSignal = null;
+        this.betaChart = null;
         this.gammaPower = null;
-        this.gammaSignal = null;
+        this.gammaChart = null;
     }
 
     FrequencyPowerTable.prototype = {
@@ -38,35 +38,10 @@ define('FrequencyPowerTable', ['jquery', 'SignalNameEnum', 'Chart', 'lodash'], f
                     .append('<td>' + signalType + '</td>')
                     .append('<td>' + self.getPowerForSignalType(signalType) + '</td>')
                     .append('<td><div class="row"><canvas id="' + signalType + self.signalName + '" width="200" height="200"></canvas></div></td>');
-                self.createGraph(row.find('canvas'));
+                self[signalType + 'Chart'] = createChart(row.find('canvas'));
                 table.append(row);
             });
             return html.append(tableContainer.append(table));
-        },
-
-        createGraph : function (canvas) {
-            var dataArray = [0, 0];
-            var lineChartData = {
-                labels : ['0', '1'],
-                datasets : [{
-                    fillColor: "rgba(151,187,205,0.2)",
-                    strokeColor: "rgba(151,187,205,1)",
-                    pointColor: "rgba(151,187,205,1)",
-                    pointStrokeColor: "#fff",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(151,187,205,1)",
-                    data: dataArray
-                }]
-            };
-            var ctx = canvas[0].getContext("2d");
-            canvas.data('chart', new Chart(ctx).Line(lineChartData, {
-                responsive: false,
-                bezierCurve: false,
-                datasetFill: false,
-                pointDot: false,
-                animation: true,
-                animationSteps: 60
-            }));
         },
 
         getPowerForSignalType : function (signalType) {
@@ -75,8 +50,38 @@ define('FrequencyPowerTable', ['jquery', 'SignalNameEnum', 'Chart', 'lodash'], f
 
         getSignalForType : function (signalType) {
             return this[signalType.toLowerCase() + 'Signal'];
+        },
+
+        handleIncomingSignalUpdate : function (signalType, value) {
+            var currentChart = this[signalType + 'Chart'];
+            currentChart.addData([value], '');
         }
     };
+
+    function createChart (canvas) {
+        var dataArray = [0, 0];
+        var lineChartData = {
+            labels : ['0', '1'],
+            datasets : [{
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: dataArray
+            }]
+        };
+        var ctx = canvas[0].getContext("2d");
+        return new Chart(ctx).Line(lineChartData, {
+            responsive: false,
+            bezierCurve: false,
+            datasetFill: false,
+            pointDot: false,
+            animation: true,
+            animationSteps: 60
+        });
+    }
 
     return FrequencyPowerTable;
 });
--- webapp/static/src/main.js
+++ webapp/static/src/main.js
@@ -1,4 +1,4 @@
-require(['jquery', 'Streamer', 'FrequencyPowerTable', 'bootstrap', 'Chart'], function ($, Streamer, FrequencyPowerTable) {
+require(['jquery', 'Streamer', 'FrequencyPowerTable', 'SignalNameEnum', 'bootstrap', 'Chart'], function ($, Streamer, FrequencyPowerTable, SignalNameEnum) {
     var fpt;
     $(document).ready(function () {
         $("td").click(function () {
@@ -7,129 +7,21 @@ require(['jquery', 'Streamer', 'FrequencyPowerTable', 'bootstrap', 'Chart'], fun
                 fpt = new FrequencyPowerTable($(this).attr('id'));
                 var table = fpt.toHtmlTable();
                 $('#tables-spot').append(table);
-//                generateTable($(this));
+                createDummyChartData(fpt);
             } else {
                 removeTable($(this));
             }
         });
     });
 
-    function generateTable (classSelected) {
-        var table = '<div class="row" id="' + classSelected.attr('id') + '-table">';
-        table += '<div class="col-md-4">';
-        table += '<h3>' + classSelected.attr('id') + '</h3>';
-        table += '<table class="table table-bordered" >';
-        table += '<thead>';
-        table += '<tr>';
-        table += '<th>Frequency</th>';
-        table += '<th>Power</th>';
-        table += '<th>Power Graph</th>';
-        table += '</tr>';
-        table += '</thead>';
-        table += '<tbody>';
-        table += '<tr>';
-        table += '<td>Delta</td>';
-        table += '<td>251.03</td>';
-        table += '<td><div class="row">';
-        table += '<canvas id="Delta' + classSelected.attr('id') + '" width="200" height="200"></canvas>';
-        table += '</div></td>';
-        table += '</tr>';
-        table += '<tr>';
-        table += '<td>Theta</td>';
-        table += '<td>72.4</td>';
-        table += '<td><div class="row">';
-        table += '<canvas id="Theta-' + classSelected.attr('id') + '" width="20" height="20"></canvas>';
-        table += '</div></td>';
-        table += '</tr>';
-        table += '<tr>';
-        table += '<td>Alpha</td>';
-        table += '<td>90.68</td>';
-        table += '<td><div class="row">';
-        table += '<canvas id="Alpha' + classSelected.attr('id') + '" width="20" height="20"></canvas>';
-        table += '</div></td>';
-        table += '</tr>';
-        table += '<tr>';
-        table += '<td>Beta</td>';
-        table += '<td>200.4</td>';
-        table += '<td><div class="row">';
-        table += '<canvas id="Beta' + classSelected.attr('id') + '" width="20" height="20"></canvas>';
-        table += '</div></td>';
-        table += '</tr>';
-        table += '<tr>';
-        table += '<td>Gamma</td>';
-        table += '<td>3500.6</td>';
-        table += '<td><div class="row">';
-        table += '<canvas id="Gamma' + classSelected.attr('id') + '" width="20" height="20"></canvas>';
-        table += '</div></td>';
-        table += '</tr>';
-        table += '</tbody>';
-        table += '</table>';
-        table += '</div>';
-        table += '</div>';
-        $('#tables-spot').append(table);
-        createGraph(classSelected);
-        //get data for graph and power
-        feedGraph(classSelected);
+    function createDummyChartData(fpt) {
+        for (var i = 0; i < 20; i++)
+            fpt.handleIncomingSignalUpdate(SignalNameEnum.signalTypes.Alpha, Math.floor(Math.random() * 100))
     }
 
     function removeTable (classSelected) {
         var classToRemove = classSelected.attr('id');
         $('#' + classToRemove + '-table').remove();
-    }
-
-    function createGraph (classSelected) {
-        var dataArray = [0, 0];
-        var lineChartData = {
-            labels: ["0", "1"],
-            datasets: [
-                {
-                    label: "My Second dataset",
-                    fillColor: "rgba(151,187,205,0.2)",
-                    strokeColor: "rgba(151,187,205,1)",
-                    pointColor: "rgba(151,187,205,1)",
-                    pointStrokeColor: "#fff",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(151,187,205,1)",
-                    data: dataArray
-                }
-            ]
-        };
-
-        var graphContainer = $('#Delta' + classSelected.attr('id'));
-        var ctx = graphContainer[0].getContext("2d");
-        graphContainer.data('chart', new Chart(ctx).Line(lineChartData, {
-            responsive: false,
-            bezierCurve: false,
-            datasetFill: false,
-            pointDot: false,
-            animation: true,
-            animationSteps: 60
-        }));
-
-    }
-
-    function feedGraph (classSelected) {
-        var graphContainer = $('#Delta' + classSelected.attr('id'));
-        var count = 1;
-
-        var streamer = new Streamer();
-        streamer.connect();
-        streamer.request('val');
-
-        $('body').on('bufferUpdated', function () {
-            var value = streamer.consumeData();
-            while (value != undefined) {
-                console.log(value);
-
-                count += 2;
-                if (count >= 30 * 4) {
-                    graphContainer.data('chart').removeData();
-                }
-
-                graphContainer.data('chart').addData([value], '');
-                value = streamer.consumeData();
-            }
-        });
     }
 });
 
