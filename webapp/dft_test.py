9871c0b7f2d8f05478b031452cc87d814ebd12dc
AaronCLH <aaronchunglaihim@hotmail.com>
2015-03-07 00:06:16 -0500

    DFT test py

---
+++ webapp/dft_test.py
@@ -0,0 +1,38 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+
+# # Generate sample data
+np.random.seed(0)
+n_samples = 2000
+time = np.linspace(0, 2*np.pi, n_samples)
+
+s1 = np.sin(2 * time)  # Signal 1 : sinusoidal signal
+# s2 = np.sign(np.sin(3 * time))  # Signal 2 : square signal
+# s3 = signal.sawtooth(2 * np.pi * time)  # Signal 3: saw tooth signal
+
+f1 = (np.fft.fft(s1,128).real[0:((128/2)+1)])
+print f1
+
+plt.figure()
+colors = ['red', 'steelblue']
+# models = [X, S, S_, H]
+# names = ['Observations (mixed signal)',
+#          'True Sources',
+#          'ICA recovered signals', 
+#          'PCA recovered signals']
+plt.subplot(2, 1, 1)
+plt.title('True Sources')
+plt.plot(s1, color='red')
+plt.subplot(2, 1, 2)
+plt.title('DFT')
+plt.plot(np.linspace(0,64,65),f1, color='steelblue')
+# for ii, (model, name) in enumerate(zip(models, names), 1):
+#     plt.subplot(4, 1, ii)
+#     plt.title(name)
+#     for sig, color in zip(model.T, colors):
+#         plt.plot(sig, color=color)
+
+plt.subplots_adjust(0.09, 0.04, 0.94, 0.94, 0.26, 0.46)
+plt.show()
