5e439ae5fbdd8f4e32ee3dc981037d064e4ba7cc
Candice Pun <clpun@uwaterloo.ca>
2015-03-12 03:29:42 -0400

    Modified audio_test.py to play different audio tracks on the left and right channels.

--- webapp/audio_test.py
+++ webapp/audio_test.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python
-import pyglet
-song = pyglet.media.load('test.mp3', streaming=False)
-song.play()
-pyglet.app.run()
+import pygame
+import time
 
-# Install pyglet and AVBin before running
+pygame.init()
+
+# Load audio tracks
+track_length = 60 # 60s
+sound1 = pygame.mixer.Sound('left_channel.wav')
+sound2 = pygame.mixer.Sound('right_channel.wav')
+
+# Find the left and right channels
+chan1 = pygame.mixer.Channel(0)
+chan2 = pygame.mixer.Channel(1)
+
+# Play audio tracks
+chan1.play(sound1)
+chan1.set_volume(1, 0)
+chan2.play(sound2)
+chan2.set_volume(0, 1)
+
+time.sleep(track_length)
